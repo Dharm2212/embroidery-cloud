3e5199eb2f6c8f74e4fd6a7b005eb982021bf50c
dharm2212 <dharmkasodariya22@gmail.com>
2026-03-03 21:51:30 +0530

    esp s3_file

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const crypto = require("crypto");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
